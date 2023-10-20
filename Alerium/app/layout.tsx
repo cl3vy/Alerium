cc785af1ce6d6a411b79f4678f9b1b04cec341a2
Xhuri-Junction <148589767+Xhuri-Junction@users.noreply.github.com>
2023-10-21 01:30:02 +0200

    Removed Inter Font

--- Alerium/app/layout.tsx
+++ Alerium/app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Provider, ThemeProvider, ThemeToggle } from "@constants/components";
 import "../public/assets/fonts/fontawesome.css";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
