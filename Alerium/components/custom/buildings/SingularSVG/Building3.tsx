01bc56135fd9e5681edb553a1e142acc2ac04f61
Xhuri-Junction <148589767+Xhuri-Junction@users.noreply.github.com>
2023-10-21 22:12:20 +0200

    Fixed Client Components

--- Alerium/components/custom/buildings/SingularSVG/Building3.tsx
+++ Alerium/components/custom/buildings/SingularSVG/Building3.tsx
@@ -1,9 +1,10 @@
+"use client"
 // React Peer Dependencies
 import React from 'react';
 // Types And Interfaces
-import {SVGProps} from "@/components/custom/buildings/SvgView";
+import { SVGProps } from "@/components/custom/buildings/SvgView";
 
-const Building3 = ({ onClick:}) => {
+const SvgWithClickablePaths = ({ onClick: clickHandler }: SVGProps) => {
   return (
     <svg
       height="100%"
@@ -21,83 +22,434 @@ const Building3 = ({ onClick:}) => {
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
       <defs />
-      <a onClick={clickHandler}>
-        <path
-          d="M395.529 3.4283L891.497 3.4283L891.497 221.022L395.529 221.022L395.529 3.4283Z"
-          fill="#ffffff"
-          fillOpacity="0"
-          fillRule="nonzero"
-          opacity="1"
-          stroke="#000000"
-          strokeLinecap="butt"
-          strokeLinejoin="round"
-          strokeWidth="2.46567"
-        />
-      </a>
-      <a onClick={clickHandler}>
-        <path
-          d="M665.314 221.022L891.497 221.022L891.497 626.625L665.314 626.625L665.314 221.022Z"
-          fill="#ffffff"
-          fillOpacity="0"
-          fillRule="nonzero"
-          opacity="1"
-          stroke="#000000"
-          strokeLinecap="butt"
-          strokeLinejoin="round"
-          strokeWidth="2.46567"
-        />
-      </a>
-      <a onClick={clickHandler}>
-        <path
-          d="M891.497 626.625L856.228 656.548"
-          fill="#ffffff"
-          fillOpacity="0"
-          fillRule="nonzero"
-          opacity="1"
-          stroke="#000000"
-          strokeLinecap="butt"
-          strokeLinejoin="round"
-          strokeWidth="2.46567"
-        />
-      </a>
-      <a onClick={clickHandler}>
-        <path
-          d="M360.261 33.351L395.529 3.4283"
-          fill="#ffffff"
-          fillOpacity="0"
-          fillRule="nonzero"
-          opacity="1"
-          stroke="#000000"
-          strokeLinecap="butt"
-          strokeLinejoin="round"
-          strokeWidth="2.46567"
-        />
-      </a>
-      <a onClick={clickHandler}>
-        <path
-          d="M856.228 251.076L891.497 221.795"
-          fill="none"
-          opacity="1"
-          stroke="#000000"
-          strokeLinecap="butt"
-          strokeLinejoin="round"
-          strokeWidth="2.46567"
-        />
-      </a>
-      <a onClick={clickHandler}>
-        <path
-          d="M855.869 33.6468L890.814 4.43023"
-          fill="none"
-          opacity="1"
-          stroke="#000000"
-          strokeLinecap="butt"
-          strokeLinejoin="round"
-          strokeWidth="2.46567"
-        />
-      </a>
-      {/* Add more <a> elements for other <path> elements here */}
+      <g id="Layer-1-copy" visibility="hidden">
+        <a onClick={clickHandler}>
+          <path
+            d="M395.529 3.4283L891.497 3.4283L891.497 221.022L395.529 221.022L395.529 3.4283Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M665.314 221.022L891.497 221.022L891.497 626.625L665.314 626.625L665.314 221.022Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M891.497 626.625L856.228 656.548"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M360.261 33.351L395.529 3.4283"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M856.228 251.076L891.497 221.795"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M855.869 33.6468L890.814 4.43023"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+      </g>
+      <g id="Layer-1-copy-copy" visibility="hidden">
+        <a onClick={clickHandler}>
+          <path
+            d="M360.261 33.351L856.228 33.351L856.228 250.945L360.261 250.945L360.261 33.351Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M630.223 251.076L664.654 221.795"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M360.322 250.623L395.887 220.683"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M630.045 250.945L856.228 250.945L856.228 656.548L630.045 656.548L630.045 250.945Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M665.495 626.22L630.338 656.367"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+      </g>
+      <g id="Layer-2" visibility="hidden">
+        <a onClick={clickHandler}>
+          <path
+            d="M4.32958 108.452L251.725 108.452L251.725 730.848L4.32958 730.848L4.32958 108.452Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M22.5588 133.843L269.955 133.843L269.955 756.239L22.5588 756.239L22.5588 133.843Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M269.955 564.007L251.725 539.442"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M252.313 302.002L269.955 327.202"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M251.725 301.811L430.111 301.811L430.111 539.442L251.725 539.442L251.725 301.811Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M269.955 327.202L448.34 327.202L448.34 564.832L269.955 564.832L269.955 327.202Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M251.725 108.452L269.955 133.843"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M430.111 301.811L448.34 327.202"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M430.111 539.442L447.316 564.007"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M251.725 730.848L269.955 756.239"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M22.5588 756.239L4.32958 730.848"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M22.3594 133.8L4.32958 108.452"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+      </g>
+      <g id="Layer-3">
+        <a onClick={clickHandler}>
+          <path
+            d="M891.801 761.116L325.296 761.116L325.296 969.779L891.801 969.779L891.801 761.116Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M874.33 742.503L307.825 742.503L307.825 951.167L874.33 951.167L874.33 742.503Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M307.825 951.167L325.296 969.779"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M548.259 653.12L325.296 653.12L325.296 761.116L548.259 761.116L548.259 653.12Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M530.788 634.508L307.825 634.508L307.825 742.503L530.788 742.503L530.788 634.508Z"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M325.296 761.116L307.825 742.503"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M325.296 653.12L307.825 634.508"
+            fill="#ffffff"
+            fillOpacity="0"
+            fillRule="nonzero"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M530.788 634.508L548.259 653.12"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M530.788 742.503L548.259 761.116"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M874.33 951.167L891.801 969.779"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+        <a onClick={clickHandler}>
+          <path
+            d="M874.33 742.503L891.801 761.116"
+            fill="none"
+            opacity="1"
+            stroke="#000000"
+            strokeLinecap="butt"
+            strokeLinejoin="round"
+            strokeWidth="2.46567"
+          />
+        </a>
+      </g>
     </svg>
   );
 };
 
-export default Building3;
+export default SvgWithClickablePaths;
